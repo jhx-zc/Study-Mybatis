282956a00971b0bcf0505b52c57e14e30ada8bc6
quding <li.niu@duitang.com>
2018-01-15 23:27:37 +0800

    init mybatis源码阅读环境

---
+++ REAMME.md
@@ -0,0 +1,5 @@
+
+# Mybatis源码阅读学习使用
+
+Mybatis版本 3.4.6-SNAPSHOT
+
---
+++ mybatis-demo/src/main/java/com/itoolshub/mybatis/studymybatis/StudyMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.itoolshub.mybatis.studymybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMybatisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudyMybatisApplication.class, args);
+  }
+
+}
